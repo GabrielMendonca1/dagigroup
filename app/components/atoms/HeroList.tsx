b0b54a0bc3cc84923463dbe7b51c856dbc323542
GabrielMendonca1 <garimeipod@gmail.com>
2023-09-26 12:52:46 -0300

    v2.0.5

--- app/components/atoms/HeroList.tsx
+++ app/components/atoms/HeroList.tsx
@@ -6,28 +6,28 @@ const HeroList = () => {
             <div className="text-3xl md:text-4xl">🛠️</div>
             <div>
               <h3 className="text-lg md:text-xl font-bold">Development</h3>
-              <p className="text-sm md:text-base">Porttitor tempor consequat tristique faucibus netus massa. Auctor nibh faucibus felis mauris vitae.</p>
+              <p className="text-sm md:text-base font-light">Porttitor tempor consequat tristique faucibus netus massa. Auctor nibh faucibus felis mauris vitae.</p>
             </div>
           </li>
           <li className="flex items-center space-x-4">
             <div className="text-3xl md:text-4xl">📱</div>
             <div>
               <h3 className="text-lg md:text-xl font-bold">Responsive Design</h3>
-              <p className="text-sm md:text-base">Erat sed ut porta ultrices. Tincidunt vulputate justo, interdum imperdiet viverra in non, eu. In tortor odio in </p>
+              <p className="text-sm md:text-base font-light">Erat sed ut porta ultrices. Tincidunt vulputate justo, interdum imperdiet viverra in non, eu. In tortor odio in </p>
             </div>
           </li>
           <li className="flex items-center space-x-4">
             <div className="text-3xl md:text-4xl">🎨</div>
             <div>
               <h3 className="text-lg md:text-xl font-bold">UI/UX Design</h3>
-              <p className="text-sm md:text-base">Massa donec vivamus ultrices nascetur velit egestas. Augue facilisis sed at mattis ac auctor a feugiat sed. </p>
+              <p className="text-sm md:text-base font-light">Massa donec vivamus ultrices nascetur velit egestas. Augue facilisis sed at mattis ac auctor a feugiat sed. </p>
             </div>
           </li>
           <li className="flex items-center space-x-4">
             <div className="text-3xl md:text-4xl">🖥️</div>
             <div>
               <h3 className="text-lg md:text-xl font-bold">Backend</h3>
-              <p className="text-sm md:text-base">Tincidunt laoreet ultricies vulputate congue sagittis dignissim orci, sapien ridiculus. Magna tincidunt arcu a.</p>
+              <p className="text-sm md:text-base font-light">Tincidunt laoreet ultricies vulputate congue sagittis dignissim orci, sapien ridiculus. Magna tincidunt arcu a.</p>
             </div>
           </li>
         </ul>
