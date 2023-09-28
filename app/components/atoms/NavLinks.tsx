45cae019f56d8bce18e4697d9801e585be829871
GabrielMendonca1 <garimeipod@gmail.com>
2023-09-28 10:23:26 -0300

    28/09/2023

--- app/components/atoms/NavLinks.tsx
+++ app/components/atoms/NavLinks.tsx
@@ -1,10 +1,10 @@
 const NavLinks = () => {
   return(
     <nav className="hidden md:flex gap-8">
-      <a href="#home" className="px-4 py-4 rounded-md hover:border-gray-500 hover:bg-gray-400">Home</a>
-      <a href="#sobre" className="px-4 py-4 rounded-md hover:border-gray-500 hover:bg-gray-400">Sobre</a>
-      <a href="#servicos" className="px-4 py-4 rounded-md hover:border-gray-500 hover:bg-gray-400">Serviços</a>
-      <a href="#contato" className="px-4 py-4 rounded-md hover:border-gray-500 hover:bg-gray-400">Contato</a>
+      <a href="#home" className="px-3 py-3 rounded-md hover:border-gray-300 hover:bg-gray-200">Home</a>
+      <a href="#sobre" className="px-3 py-3 rounded-md hover:border-gray-300 hover:bg-gray-200">Sobre</a>
+      <a href="#servicos" className="px-3 py-3 rounded-md hover:border-gray-300 hover:bg-gray-200">Serviços</a>
+      <a href="#contato" className="px-3 py-3 rounded-md hover:border-gray-300 hover:bg-gray-200">Contato</a>
     </nav>
   );
 };
