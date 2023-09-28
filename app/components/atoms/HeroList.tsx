224a3eefafa277f08f9bc9c4015c1d295f7b876e
GabrielMendonca1 <garimeipod@gmail.com>
2023-09-28 10:14:51 -0300

    28/09/2023

--- app/components/atoms/HeroList.tsx
+++ app/components/atoms/HeroList.tsx
@@ -1,33 +1,33 @@
 const HeroList = () => {
     return (
-      <div className="flex flex-col items-center justify-center w-full h-full px-4 text-center md:px-0 md:text-left">
+      <div className="flex flex-col items-center justify-center w-full h-full text-center md:px-0 md:text-left">
         <ul className="space-y-16">
-          <li className="flex items-center space-x-4">
-            <div className="text-3xl md:text-4xl">🛠️</div>
-            <div>
+          <li className="flex items-center text-left space-x-4 p-2">
+            <div className="text-2xl md:text-2xl">🛠️</div>
+            <div className="w-[70%]">
               <h3 className="text-lg md:text-xl font-bold">Development</h3>
-              <p className="text-sm md:text-base font-light">Porttitor tempor consequat tristique faucibus netus massa. Auctor nibh faucibus felis mauris vitae.</p>
+              <p  className="text-xs md:text-sm font-light">Porttitor tempor consequat tristique faucibus netus massa. Auctor nibh faucibus felis mauris vitae.</p>
             </div>
           </li>
-          <li className="flex items-center space-x-4">
-            <div className="text-3xl md:text-4xl">📱</div>
-            <div>
+          <li className="flex items-center text-left space-x-4 p-2">
+            <div className="text-2xl md:text-2xl">📱</div>
+            <div className="w-[70%]">
               <h3 className="text-lg md:text-xl font-bold">Responsive Design</h3>
-              <p className="text-sm md:text-base font-light">Erat sed ut porta ultrices. Tincidunt vulputate justo, interdum imperdiet viverra in non, eu. In tortor odio in </p>
+              <p  className="text-xs md:text-sm font-light">Erat sed ut porta ultrices. Tincidunt vulputate justo, interdum imperdiet viverra in non, eu. In tortor odio in </p>
             </div>
           </li>
-          <li className="flex items-center space-x-4">
-            <div className="text-3xl md:text-4xl">🎨</div>
-            <div>
-              <h3 className="text-lg md:text-xl font-bold">UI/UX Design</h3>
-              <p className="text-sm md:text-base font-light">Massa donec vivamus ultrices nascetur velit egestas. Augue facilisis sed at mattis ac auctor a feugiat sed. </p>
+          <li className="flex items-center text-left space-x-4 p-2">
+            <div className="text-2xl md:text-2xl">🎨</div>
+            <div className="w-[70%]">
+              <h3 className="text-lg md:text-lg font-bold">UI/UX Design</h3>
+              <p  className="text-xs md:text-sm font-light">Massa donec vivamus ultrices nascetur velit egestas. Augue facilisis sed at mattis ac auctor a feugiat sed. </p>
             </div>
           </li>
-          <li className="flex items-center space-x-4">
-            <div className="text-3xl md:text-4xl">🖥️</div>
-            <div>
+          <li className="flex items-center text-left space-x-4 p-2">
+            <div className="text-2xl md:text-2xl">🖥️</div>
+            <div className="w-[70%]">
               <h3 className="text-lg md:text-xl font-bold">Backend</h3>
-              <p className="text-sm md:text-base font-light">Tincidunt laoreet ultricies vulputate congue sagittis dignissim orci, sapien ridiculus. Magna tincidunt arcu a.</p>
+              <p  className="text-xs md:text-sm font-light">Tincidunt laoreet ultricies vulputate congue sagittis dignissim orci, sapien ridiculus. Magna tincidunt arcu a.</p>
             </div>
           </li>
         </ul>
--- app/components/atoms/ImgCarrousel.tsx
+++ app/components/atoms/ImgCarrousel.tsx
@@ -1,18 +1,15 @@
 const ImgCarrousel = () => {
-    return (
-      <section className="py-8 md:py-12 bg-white dark:bg-black flex flex-col md:flex-row">
-        <div className="container mx-auto px-4 flex flex-col md:flex-row">
-          <div className=" w-auto md:w-[400px] lg:w-[1200px] xl:w-[1200px] md:h-[492] flex items-center justify-center">
-            <img 
-              src="https://images.unsplash.com/photo-1541873676-a18131494184?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2662&q=80" 
-              alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-              className="object-cover object-center w-[660px] h-[600px] md:border md:border-gray-300 md:shadow-md" 
-            />
-          </div>
-        </div>
-      </section>
-    );
-  };
-  
-  export default ImgCarrousel;
-  
+  return (
+    <section className="py-4 md:py-8 bg-white dark:bg-black flex items-center justify-center">
+      <div className="w-full max-w-xs md:max-w-sm lg:max-w-md xl:max-w-lg px-4 py-4">
+        <img 
+          src="https://images.unsplash.com/photo-1541873676-a18131494184?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2662&q=80" 
+          alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          className="w-full h-auto object-cover object-center rounded shadow-md" 
+        />
+      </div>
+    </section>
+  );
+};
+
+export default ImgCarrousel;
